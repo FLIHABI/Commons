4e44e11bb76919d17277bf4961fea989426668d5
Snaipe <franklinmathieu@gmail.com>
2015-04-09 12:40:16 +0200

    Switched bytecodes from defines to enum

--- include/commons/asm/bytecodes.hh
+++ include/commons/asm/bytecodes.hh
@@ -7,7 +7,8 @@ VM management
 ------------------------------------------------
 */
 
-#define OP_HALT 0x00
+enum Bytecode {
+    OP_HALT = 0x00,
 
 /*
 ------------------------------------------------
@@ -15,10 +16,10 @@ Stack and register management
 ------------------------------------------------
 */
 
-#define OP_POP 0x13
-#define OP_PUSH 0x17
-#define OP_PUSHR 0x18
-#define OP_POPR 0x19
+    OP_POP = 0x13,
+    OP_PUSH = 0x17,
+    OP_PUSHR = 0x18,
+    OP_POPR = 0x19,
 
 /*
 ------------------------------------------------
@@ -26,11 +27,11 @@ Arithmetic
 ------------------------------------------------
 */
 
-#define OP_ADD 0x20
-#define OP_SUB 0x21
-#define OP_MUL 0x22
-#define OP_DIV 0x23
-#define OP_MOD 0x24
+    OP_ADD = 0x20,
+    OP_SUB = 0x21,
+    OP_MUL = 0x22,
+    OP_DIV = 0x23,
+    OP_MOD = 0x24,
 
 /*
 ------------------------------------------------
@@ -38,7 +39,7 @@ Tests
 ------------------------------------------------
 */
 
-#define OP_CMP 0x30
+    OP_CMP = 0x30,
 
 /*
 ------------------------------------------------
@@ -46,16 +47,16 @@ Branching
 ------------------------------------------------
 */
 
-#define OP_CALL 0x40
-#define OP_CALLR 0x49
-#define OP_RET 0x41
-#define OP_JMP 0x42
-#define OP_JE 0x43
-#define OP_JL 0x44
-#define OP_JG 0x45
-#define OP_JNE 0x46
-#define OP_JLE 0x47
-#define OP_JGE 0x48
+    OP_CALL = 0x40,
+    OP_CALLR = 0x49,
+    OP_RET = 0x41,
+    OP_JMP = 0x42,
+    OP_JE = 0x43,
+    OP_JL = 0x44,
+    OP_JG = 0x45,
+    OP_JNE = 0x46,
+    OP_JLE = 0x47,
+    OP_JGE = 0x48,
 
 /*
 ------------------------------------------------
@@ -63,7 +64,9 @@ Object management
 ------------------------------------------------
 */
 
-#define OP_CREATE 0x50
-#define OP_DELETE 0x51
+    OP_CREATE = 0x50,
+    OP_DELETE = 0x51,
 
-#endif /* end of include guard: BYTECODE_HH */
+};
+
+#endif /* !BYTECODE_HH */
