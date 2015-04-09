fddb08cd071e6fcd040b1cd51c0e51785387e714
Snaipe <franklinmathieu@gmail.com>
2015-04-09 19:03:37 +0200

    Added vector stream facility

---
+++ include/commons/utils/vector_stream.hxx
@@ -0,0 +1,21 @@
+#ifndef VECTOR_STREAM_HXX_
+# define VECTOR_STREAM_HXX_
+
+# include <vector>
+
+namespace vector_stream {
+
+    template <typename T>
+    void write(std::vector<char>& vec, const T v) {
+        for (int i = 0; i != sizeof (T) * 8; i += 8)
+            vec.push_back((v >> i) & 0xFF);
+    }
+}
+
+template <typename T>
+std::vector<char>& operator<<(std::vector<char>& vec, const T v) {
+    vector_stream::write(vec, v);
+    return v;
+}
+
+#endif /* !VECTOR_STREAM_HXX_ */
